114a33d0ac335e3487c1b5d2cd2f68b19454d55e
Heiye-NMW <zhanghaoyu19281@outlook.com>
2025-02-22 01:10:32 +0800

    deleted:    ._index.html 	modified:   bilidown.html 	modified:   css/404nf.css 	modified:   css/bilidown.css 	modified:   css/index.css 	modified:   css/index.html 	modified:   doc/index.html 	modified:   doc/index/css/G100A.css 	modified:   doc/index/css/padmain.css 	modified:   doc/index/index.html 	modified:   doc/index/js/jquery.min.js 	modified:   doc/index/js/jquery.nav.js 	modified:   doc/index/js/main.js 	modified:   index.html 	modified:   js/index.js 	new file:   js/tyjs.js 	new file:   license.file 	new file:   software/css/G100A.css 	new file:   software/css/padmain.css 	new file:   software/img/C35/1.png 	new file:   software/img/C35/energy-label.png 	new file:   software/img/K5/1.png 	new file:   software/img/K5/energy-label.png 	new file:   software/img/favicon.ico 	new file:   software/img/guarantee.png 	new file:   software/img/i10.jpg 	new file:   software/img/icon_black.png 	new file:   software/img/icon_circle.png 	new file:   software/img/logo.png 	new file:   software/img/main.png 	new file:   software/img/notice-logo.png 	new file:   software/img/notice2.png 	new file:   software/img/open.png 	new file:   software/img/return.png 	new file:   software/img/sign.png 	new file:   software/img/warning.png 	new file:   software/img/zhiliu.png 	new file:   "software/img/\344\272\244\345\217\211.png" 	new file:   "software/img/\346\225\231\350\202\262\347\233\264\346\222\255\345\271\263\346\235\277code.png" 	new file:   "software/img/\346\225\231\350\202\262\347\247\221\346\212\200logo.png" 	new file:   "software/img/\346\240\241\345\233\255\345\215\253\345\243\253.jpg" 	modified:   software/index.html 	new file:   software/js/jquery.min.js 	new file:   software/js/jquery.nav.js 	new file:   software/js/main.js 	new file:   sources/Open-source_files_and_certificates.zip 	modified:   sponsor.html 	modified:   tgserver/index.html 	modified:   tgserver/syc/css/padmain.css 	modified:   tgserver/syc/index.html

--- js/index.js
+++ js/index.js
@@ -1,14 +1,27 @@
-document.getElementById("dia").showModal(); 
-
-
 let texts = [
     '欢迎访问王子的小窝，祝你开心啊。',
-    '我就是我，世间第一等，此间最上乘。',
+    '若命运不公，便和它奋斗到底。',
+    '若前方无路，我便踏出一条路；若天地不容，我便扭转这乾坤。',
+    '我命由我不由天，是魔是仙，我自己说了算！',
+    '别人的看法都是狗屁，你是谁只有你自己说了才算。',
+    '我若成佛，天下无魔；我若成魔，佛奈我何。',
+    '人心中的成见是一座大山，任你怎么努力都休想搬动。',
+    '难道你还想改变这世界？我想试试。',
+    '以前我一直在躲、在藏，直到无路可走，我才明白该如何面对这个世界。',
+    '你自诩照世明灯，却干着恃强凌弱、祸乱人间的勾当。',
+    '无论你是“魔”是“妖”还是“仙”，你都是我儿，每一天与你在一起都是快乐的。',
+    '从来生死都看淡，专和老天对着干。我命由我不由天，小爷成魔不成仙。',
+    '哪怕肉身重塑，哪怕万丈火焰，我也是逆鳞一片，燃我自己。',
+    '什么神仙妖魔，不过是禁锢异族命运的枷锁。',
+    '难道你还想改变这世界？我想试试。',
+    '安静的做，成功了再说。',
+    '欲买桂花同载酒，终不似，少年游。',
+    '虽不能至，心向往之。',
     '安静的做，成功了再说。',
     '松花酿酒，春水煎茶。',
-    '虽不能至，心向往之。'
+    '虽不能至，心向往之。',
+    '无人扶我青云志，我自踏雪至山巅。'
 ]
-
 let textsIndex = 0;
 let index = 0;
 let dom = document.querySelector(".prince-website .prince .prince-info p");
@@ -16,8 +29,9 @@ let dom = document.querySelector(".prince-website .prince .prince-info p");
 let printText = async (text, dom) => {
     let index = 0
     let useTimeCount = 0;
-    while (index !== text.length) {
-        let interval = Math.floor(Math.random() * 400);
+    let len = text.length;
+    while (index !== len) {
+        let interval = Math.floor(Math.random() * 300);
         await sleep(interval);
         useTimeCount += interval;
         dom.textContent += text.charAt(index);
@@ -29,9 +43,7 @@ let printText = async (text, dom) => {
 
 let sleep = (interval) => {
     return new Promise((success) => {
-        setTimeout(() => {
-            success()
-        }, interval)
+        setTimeout(() => { success() }, interval)
     })
 }
 
---
+++ js/tyjs.js
@@ -0,0 +1,7 @@
+function redirect(url) {
+    window.location.href = url; // 替换为目标页面的URL
+}
+
+function goBack() {
+    window.history.back(); // 返回到浏览器历史记录中的前一个页面
+}
